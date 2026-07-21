6b481f9052bced9a5d00a49eb60e1c9960d30a4e
KtesaTech <ktesatech.reception.co@gmail.com>
2026-07-21 13:19:00 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Horeb-Animal-Farm/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
